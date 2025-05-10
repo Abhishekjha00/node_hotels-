4e6b1d142b6f09c850528b21c01219ff2f80ed56
Abhishekjha00 <ajha98169@gmail.com>
2025-05-10 18:47:52 +0530

    shift mongodb to online server

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express'); //express ki requirement or import kar rhe h
 
 const app = express()    // express ko import kar liya app me, app is an instance of express (blueprint), app ki help se ab sab karenge 
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());  //store the data in req.body me
+const PORT = process.env.PORT || 3000;
+
 
 app.get('/', (req, res) => {
   res.send('Welcome to my hotel...How can i help you?, we have to list of menu')
